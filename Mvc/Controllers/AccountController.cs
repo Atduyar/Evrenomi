8a69f2ee2ebddb1c491fee345a9dfb2822e560ae
= <=>
2020-10-22 00:26:40 +0300

    Add project files.

---
+++ Mvc/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult ChangeProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ Mvc/Controllers/JoinController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Controllers
+{
+    public class JoinController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+    }
+}
